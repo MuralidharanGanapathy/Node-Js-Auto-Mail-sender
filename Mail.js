9bb4b087d23c11984b75cadcc27fda0fc7c981d3
Muralidharan Ganapathy <43808822+MuralidharanGanapathy@users.noreply.github.com>
2020-02-10 21:45:57 +0530

    Create Mail.js

---
+++ Mail.js
@@ -0,0 +1,55 @@
+var fs = require('fs');
+var parse = require('csv-parse');
+
+var op = "";
+
+let cron = require('node-cron');
+  let nodemailer = require('nodemailer');
+
+
+cron.schedule('* * * * *', function(){
+
+var inputFile='samplecsv.csv';
+console.log("Processing csv file and parsing");
+  
+  let transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+          user: 'murali9942@gmail.com',
+          pass: 'Muralidharan01'
+        }
+    });
+var parser = parse({delimiter: ','}, function (err, data) {
+
+    data.forEach(function(line) {
+  
+      let mailOptions = {
+        from: 'murali9942@gmail.com',
+        to: line[2],
+        subject: 'An automated Node js message',
+        text: 
+        "Hey"+ line[0] + "\n" +"Kindly confirm if the following details are correct.\n"+
+             line[0] + line[1] + "\n" + line[2] + "\n" + line[3] + "\n" + line[4] + "\n" + line[5] +
+             "Regards,\n"+
+             "Muralidharan."
+   };
+
+
+
+  transporter.sendMail(mailOptions, function(error, info){
+        if (error) {
+          console.log(error);
+        } else {
+          console.log('Email sent: ' + info.response);
+        }
+    });
+
+    });    
+});
+ 
+
+fs.createReadStream(inputFile).pipe(parser);
+
+});
+
+  
