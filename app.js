fa7c29d4cebc3352fc9bd452b3c22a9b9b4accb1
Muralidharan Ganapathy <43808822+MuralidharanGanapathy@users.noreply.github.com>
2020-02-11 11:39:29 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,2 +1,12 @@
-var a = "hello!";
-console.log(a);
+const http = require('http');
+const port = process.env.PORT || 3000
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/html');
+  res.end('<h1>Hello World</h1>');
+});
+
+server.listen(port,() => {
+  console.log(`Server running at port `+port);
+});
